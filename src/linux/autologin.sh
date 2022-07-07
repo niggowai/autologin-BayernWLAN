88036333906fca3c33433dcd9712b848b879054b
niggowai <68733302+niggowai@users.noreply.github.com>
2022-07-07 15:22:21 +0200

    not needed for windows

--- src/linux/autologin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while true
-do
-jsondata=$(curl -s https://hotspot.vodafone.de/api/v4/session)
-session=${jsondata:18:32}
-nichts=$(curl -s "https://hotspot.vodafone.de/api/v4/login?loginProfile=6&accessType=termsOnly&sessionID=${session}&action=redirect&portal=bayern")
-sleep 10
-done
